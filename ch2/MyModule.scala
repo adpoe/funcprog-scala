4f2bf2c4ccc82b0b878453389df7c1686932925b
Tony Poerio <tony@tonypoer.io>
2017-06-14 00:06:19 -0400

    Got Intellij Env setup. Did ex 2.1

--- ch2/MyModule.scala
+++ ch2/MyModule.scala
@@ -12,7 +12,30 @@ object MyModule { // a singleton object --> "defines a class and its only instan
     val msg = "the absolute value of %d is %d"
     msg.format(x, abs(x))
  }
-  
+
+  def factorial(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, acc: Int): Int =
+      if (n <= 0) acc  // return acc once n <= 0
+      else go(n-1, n*acc)
+
+    go(n, 1)
+  }
+
+  // Ex 2.1
+  def fibonacci(n: Int): Int = {
+    // write this
+    def fibHelper(x: Int, prevPrev: Int = 0, prev: Int = 1): Int = x match {
+      case 0 => prevPrev
+      case 1 => prev
+      case _ =>  fibHelper(x-1, prev, prevPrev + prev)
+    }
+
+    fibHelper(n)
+  }
+
   def main(args: Array[String]): Unit = // Unit == void 
     println(formatAbs(-42))
+    println(factorial(7))
+    println(fibonacci(10))
 }
